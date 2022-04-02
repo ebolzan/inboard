b74a5df7758bb3ff1d98b96819b7c109639dfa02
ebolzan <ebolzan@inf.ufsm.br>
2022-04-02 00:14:08 -0300

    refactor(admin/devices): list fields and list images in same view

--- devices/admin.py
+++ devices/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Device, ImageDevice
 
-admin.site.register(Device)
+
+class ImageDeviceInstanceInline(admin.TabularInline):
+    model = ImageDevice
+
+    readonly_fields = ('thumbnail',)
+
+    class Media:
+        css = {
+            'all': ('devices/css/style.css',)
+        }
+
+
+
 admin.site.register(ImageDevice)
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('tag_id', 'name', 'type', 'region', 'local', 'team_owner', 'serial_number', 'asset_tag', 'shorDescription')
+    inlines = [ImageDeviceInstanceInline]
+
+    def shorDescription(self, obj):
+        return obj.observation[:30]
+
+    shorDescription.short_description = 'Descrição'
+
+
 
 # Register your models here.
